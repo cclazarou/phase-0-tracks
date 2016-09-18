c80caa0638ea29ca233a327d7c8f8cb6ba4aba03
cclazarou <candacelazarou@gmail.com>
2016-09-18 01:29:09 -0700

    ask employee for allergy information

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,5 +1,15 @@
 require 'date'
 
+#VARIABLES
+  conclusion = "Results inconclusive"
+  current_year = Date.today.year
+
+  #need to check if today is last day of year
+  is_last_day_of_year = Date.today.year != Date.today.next_day.year
+  age_is_right = nil
+  allergy = nil
+
+# SURVEY
 puts "How many employees will be processed?"
 number_of_employees = gets.chomp.to_i
 
@@ -20,17 +30,30 @@ for i in 1..number_of_employees
   puts "Would you like to enroll in the company's health insurance (y or n)?"
   health_insurance = gets.chomp
 
-  #assign value to variables
-  conclusion = "Results inconclusive"
-  current_year = Date.today.year
-  birth_year_from_age = current_year - age.to_i
-  #need to check if today is last day of year
-  is_last_day_of_year = Date.today.year != Date.today.next_day.year
-  #if staff surveyed before birthday, need to allow for leeway in stated birth year (1 year earlier)
-  birth_year_from_age_allowance = birth_year_from_age-1
-  age_is_right = nil
+  puts "Please tell us what allergies you have.  If you have multiple allergies, type one allergy, hit enter/return, then type the next allergy, etc.  Please type 'done' when you are finished."
+  allergy = gets.chomp
 
-  #AGE CHECK
+  # continue asking for allergies
+  while allergy != "done"
+
+    puts "Next allergy:"
+    allergy = gets.chomp
+
+    if allergy == "sunshine"
+      conclusion = "Probably a vampire"
+      break
+    end
+
+  end
+
+#AGE CHECK
+
+  #calculate birth year according to age recieved in survey
+  birth_year_from_age = current_year - age.to_i
+
+  #if staff member is surveyed before birthday, allow for leeway in stated birth year (1 year earlier)
+  birth_year_from_age_allowance = birth_year_from_age-1
+
   if is_last_day_of_year
     if
       birth_year_from_age == birth_year_from_survey
@@ -46,7 +69,7 @@ for i in 1..number_of_employees
     end
   end
 
-  #CONDITIONS
+#CONDITIONS
   if age_is_right && (garlic_order == "y" || health_insurance == "y")
     conclusion = "Probably not a vampire"
   end
@@ -64,17 +87,6 @@ for i in 1..number_of_employees
   end
 
   puts conclusion
-
-  # puts "is last day of year?"
-  # puts is_last_day_of_year
-  # puts "is birth year from age same as birth year from survey?"
-  # puts birth_year_from_age == birth_year_from_survey
-  # puts "what is the birth year from age"
-  # puts birth_year_from_age
-  # puts "what is birth year from age allowance"
-  # puts birth_year_from_age_allowance
-  # puts "age is right:"
-  # puts age_is_right
 
 #end number of employees FOR loop
 end
