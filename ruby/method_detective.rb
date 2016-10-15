4395ae11ca8cacd56e1e9e2f9d3947606365e72f
cclazarou <candacelazarou@gmail.com>
2016-10-14 18:26:00 -0700

    add method that removes first character in string

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,74 +4,155 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
+puts "CULPRIT A"
 #method inverts case
 varA = "iNvEsTiGaTiOn"
-culpritA1 = varA.swapcase
+#set testing variable
+varA_copy = varA
+
+culpritA1 = varA_copy.swapcase
 puts "output: #{culpritA1}"
-puts "original after output: #{varA}"
-#swapcase! changes the receiver to match the method's return
-culpritA2 = varA.swapcase!
+puts "original after output: #{varA_copy}"
+
+#same as .swapcase but changes the input to match return
+#reset testing variable
+varA_copy = varA
+culpritA2 = varA_copy.swapcase!
 puts "output: #{culpritA2}"
-puts "original after output: #{varA}"
+puts "original after output: #{varA_copy}"
 # => “InVeStIgAtIoN”
 
-#or method finds character in string and substitutes with other character/s
+puts "CULPRIT B"
+#method finds substring and substitutes with new substring
 varB = "zom"
-culpritB1 = varB.sub("o","oo")
+varB_copy = varB
+
+culpritB1 = varB_copy.sub("o","oo")
 puts "output: #{culpritB1}"
-puts "original after output: #{varB}"
-culpritB2 = varB.sub!("o","oo")
+puts "original after output: #{varB_copy}"
+
+varB_copy = varB
+culpritB2 = varB_copy.sub!("o","oo")
 puts "output: #{culpritB2}"
-puts "original after output: #{varB}"
+puts "original after output: #{varB_copy}"
 # => “zoom”
 
+puts "CULPRIT C"
 # method centers string and pads left and right if width given is larger than input string
 varC = "enhance"
-culpritC1 = varC.center(15)
+varC_copy = varC
+
+culpritC1 = varC_copy.center(15)
 puts "output: #{culpritC1}"
-puts "original after output: #{varC}"
+puts "original after output: #{varC_copy}"
 # => "    enhance    "
 
 # method changes lower to upper case in string
 varD = "Stop! You're under arrest!"
-culpritD1 = varD.upcase
+varD_copy = varD
+
+puts "CULPRIT D"
+#method changes all lower case in string to upper case
+culpritD1 = varD_copy.upcase
 puts "output: #{culpritD1}"
 puts "original after output: #{varD}"
-culpritD2 = varD.upcase!
+
+varD_copy = varD
+culpritD2 = varD_copy.upcase!
 puts "output: #{culpritD2}"
-puts "original after output: #{varD}"
-# "Stop! You’re under arrest!".<???>
+puts "original after output: #{varD_copy}"
 # => "STOP! YOU’RE UNDER ARREST!"
 
+puts "CULPRIT E"
 # method that appends new string to old string
 varE = "the usual"
-culpritE1 = varE + " suspects"
+varE_copy = varE
+
+culpritE1 = varE_copy + " suspects"
 puts "output: #{culpritE1}"
-puts "original after output: #{varE}"
-culpritE2 = varE << " suspects"
+puts "original after output: #{varE_copy}"
+
+varE_copy = varE
+culpritE2 = varE_copy << " suspects"
 puts "output: #{culpritE2}"
-puts "original after output: #{varE}"
+puts "original after output: #{varE_copy}"
 #=> "the usual suspects"
 
+puts "CULPRIT F"
 #method that prepends new string to old string
 varF = " suspects"
-culpritF1 = varF.prepend("the usual")
+varF_copy = varF
+
+culpritF1 = varF_copy.prepend("the usual")
 puts "output: #{culpritF1}"
-puts "original after output: #{varF}"
+puts "original after output: #{varF_copy}"
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts "CULPRIT G"
+#method that chops off last character in string"
+varG = "The case of the disappearing last letter"
+varG_copy = varG
+
+culpritG1 = varG_copy.chop
+puts "output: #{culpritG1}"
+puts "original after output: #{varG_copy}"
+
+#same as .chop but returns nil if input string is empty
+varG_copy = varG
+culpritG2 = varG_copy.chop!
+
+puts "output: #{culpritG2}"
+puts "original after output: #{varG_copy}"
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "CULPRIT H"
+varH = "The mystery of the missing first letter"
+varH_copy = varH
+
+#deletes a substring that you specify
+culpritH1 = varH_copy.delete("T")
+puts "output: #{culpritH1}"
+puts "original after ouput: #{varH_copy}"
+
+varH_copy = varH
+culpritH2 = varH_copy.delete!("T")
+puts "output: #{culpritH2}"
+puts "original after ouput: #{varH_copy}"
+
+#returns a substring at a given index. BUT it deletes a substring at a specified index, so I'm counting it as a possible culprit
+varH_copy = varH
+culpritH3 = varH_copy.slice!(0)
+puts "output: #{culpritH3} - see comment in code"
+puts "original after ouput: #{varH_copy} - in this case, the input string becomes the desired new string, but the actual return of the method is not what we want"
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+puts "CULPRIT I"
+varI = "The mystery of the missing first letter"
+varI_copy = varI
 
-# "z".<???>
-# => 122
-# (What is the significance of the number 122 in relation to the character z?)
+# culpritI1 = varI_copy.
+# puts "output: #{culpritI1}"
+# puts "original after ouput: #{varI_copy}"
+# # "Elementary,    my   dear        Watson!".<???>
+# # => "Elementary, my dear Watson!"
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+# puts "CULPRIT J"
+# varJ = "The mystery of the missing first letter"
+# varJ_copy = varJ
+
+# culpritJ1 = varJ_copy.
+# puts "output: #{culpritJ1}"
+# puts "original after ouput: #{varJ_copy}"
+# # "z".<???>
+# # => 122
+# # (What is the significance of the number 122 in relation to the character z?)
+
+# puts "CULPRIT K"
+# varK = "The mystery of the missing first letter"
+# varK_copy = varK
+
+# culpritK1 = varK_copy.
+# puts "output: #{culpritK1}"
+# puts "original after ouput: #{varK_copy}"
+# # "How many times does the letter 'a' appear in this string?".<???>
+# # => 4
