0c2a9be5971115366dedbcfbf9c3d200c802bb3d
cclazarou <candacelazarou@gmail.com>
2016-10-16 15:32:26 -0700

    5.3r1 add candy_types array and candy_eaters hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -8,8 +8,13 @@ end
 
 status{|candy1,candy2| puts "candy1: #{candy1}, candy2: #{candy2}"}
 
-# candy_types = ["chocolate","candy cane","gummy bear"]
+candy_types = ["chocolate","candy cane","gummy bear"]
 
+candy_eaters = {
+  tom: "hersheys",
+  jerry: "reeses",
+  mary: "hot cheetos mac n' cheese"
+}
 
 
 
