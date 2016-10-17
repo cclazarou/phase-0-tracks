f87b51a64394fed6ba3077aa96016d96199e1dcf
cclazarou <candacelazarou@gmail.com>
2016-10-16 23:35:42 -0700

    prompt user for client info

---
+++ ruby/hashes.rb
@@ -0,0 +1,18 @@
+#prompt user for client info
+#convert input to appropriate data type
+#print hash
+#prompt user to correct client info if necessary
+#print hash again, exit
+
+puts "Please enter client information"
+puts "name:"
+name = gets.chomp
+
+puts "age:"
+age = gets.chomp
+
+puts "decor theme:"
+theme = gets.chomp
+
+puts "are they a jerk? please type y or n"
+theme = gets.chomp
