b4487eab211bf99e75ac8501ffdeb6a7fbc71888
cclazarou <candacelazarou@gmail.com>
2016-10-16 23:11:21 -0700

    add bubble sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -14,3 +14,29 @@ end
 puts fib(6)
 puts fib(100).last == 218922995834555169026
 
+#BUBBLE SORT
+#iterate through array, comparing current index to next index, swapping if next index is less than current index
+#do this until you get to the next to the last index
+#repeat the whole process until no swaps are made
+
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+  array
+end
+
+some_array = [8,9,2,1,21]
+puts "array before bubble sort #{some_array}"
+
+bubble_sort(some_array)
+
+puts "array after bubble sort #{some_array}"
