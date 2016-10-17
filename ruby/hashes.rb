b49a1c5098eb60c7e6424bd81988b43859a379b1
cclazarou <candacelazarou@gmail.com>
2016-10-16 23:46:36 -0700

    5.2 R3 convert user input to appropriate data types

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,9 +1,20 @@
 #prompt user for client info
 #convert input to appropriate data type
+#create hash with variables given
 #print hash
 #prompt user to correct client info if necessary
 #print hash again, exit
 
+#boolean conversion method
+def to_b(str)
+  if str == "y"
+    return true
+  else
+    return false
+  end
+end
+
+#prompt user for client info
 puts "Please enter client information"
 puts "name:"
 name = gets.chomp
@@ -15,4 +26,13 @@ puts "decor theme:"
 theme = gets.chomp
 
 puts "are they a jerk? please type y or n"
-theme = gets.chomp
+jerk = gets.chomp
+
+#type conversion
+age = age.to_i
+jerk = to_b(jerk)
+
+#create hash
+
+puts
+
