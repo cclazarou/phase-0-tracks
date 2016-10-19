136a911a4d17bd24a55a7592b6d1311ba253f2b4
cclazarou <candacelazarou@gmail.com>
2016-10-18 18:25:54 -0700

    refactored code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -7,33 +7,12 @@
   # print the list to the console [can you use one of your other methods here?] - puts will print nicely
   # create hash that will accept items in array as keys and all values will be the default value
 # output: hash
-def create_list(str)
-  quantity = 1
-
-  glist_array = str.split(" ")
-
-  puts glist_array
-
-  glist_hash = {}
-
-  glist_array.each do |item|
-    glist_hash[item] = quantity
-  end
-
-  p glist_hash
-
-end
 
 # Method to add an item to a list
 # input: item name (key value) and optional quantity (optional argument)
 # steps:
   #find create key in hash and assign quantity (if provided) as value
 # output: key value pair confirmation
-
-def add_to_list(glist,item,quantity=1)
-  glist[item] = quantity
-  p glist
-end
 
 # Method to remove an item from the list
 # input: item name
@@ -42,11 +21,6 @@ end
   #delete that key value pair
 # output: updated list
 
-def remove_item(glist,item)
-  glist.delete(item)
-  p glist
-end
-
 # Method to update the quantity of an item
 # input: name of item, new quantity
 # steps:
@@ -54,10 +28,10 @@ end
   #assign new quantity as value
 # output: updated list
 
-def new_quantity(glist,item,new_qty)
-  glist[item] = new_qty
-  p glist
-end
+# def new_quantity(glist,item,new_qty)
+#   glist[item] = new_qty
+#   p glist
+# end
 
 # Method to print a list and make it look pretty
 # input: list (hash)
@@ -65,14 +39,27 @@ end
   #iterate through hash and print each kb pair on new line
 # output: pretty list
 
+def create_list(str)
+  glist_array = str.split(" ")
+  glist_hash = {}
+  glist_array.each { |item| glist_hash[item] = 1 }
+  return glist_hash
+end
+
+def update_list(glist,item,quantity=1)
+  glist[item] = quantity
+end
+
+def remove_item(glist,item)
+  glist.delete(item)
+end
+
 def pretty_princess(glist)
-  glist.each do |k,v|
-    puts "#{k}: #{v}"
-  end
+  glist.each { |item,quantity| puts "#{item}: #{quantity}" }
 end
 
 list = create_list("apples oranges bananas")
-add_to_list(list,"plums",8)
+update_list(list,"plums",8)
+update_list(list, "umbrellas")
 remove_item(list,"oranges")
-new_quantity(list, "apples", 100)
 pretty_princess(list)
