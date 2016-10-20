66b18e203be553045afe7531c63bfad9d9914655
cclazarou <candacelazarou@gmail.com>
2016-10-20 14:54:54 -0700

    add speak, roll_over, dog_years, and play_dead instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,11 +5,34 @@ class Puppy
     toy
   end
 
+  def speak(number)
+    i = 0
+    while i < number
+      puts "woof"
+      i += 1
+    end
+  end
 
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    dog_equiv = human_years * 7
+    puts dog_equiv
+  end
+
+  def play_dead
+    puts "*plays dead*"
+  end
 
 end
 
 ralph = Puppy.new
 
 ralph.fetch("frisbee")
+ralph.speak(3)
+ralph.roll_over
+ralph.dog_years(2)
+ralph.play_dead
 
