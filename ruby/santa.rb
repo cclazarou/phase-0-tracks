3421119acc208d7a70a3e51e9a804691476a0119
cclazarou <candacelazarou@gmail.com>
2016-10-21 21:34:57 -0700

    add gender, ethnicity, reindeer_ranking, and age instance variables to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,10 @@
 class Santa
-  def initialize
+  def initialize(gender,ethnicity)
     "Initializing Santa instance"
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -10,4 +14,34 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}"
   end
+
+  def about_instance
+    puts "gender #{@gender}"
+    puts "ethnicity #{@ethnicity}"
+    puts "reindeer_ranking #{@reindeer_ranking}"
+    puts "age #{@age}"
+  end
+
 end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+candace = Santa.new("cisgender","filipino")
+
+candace.about_instance
+
+#prints all members of santas array along with instance variables
+puts "#{santas}"
+
+#prints Class and address
+puts "#{santas[0]}"
+
+
+
+
