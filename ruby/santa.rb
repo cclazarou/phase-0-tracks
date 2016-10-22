06561971090e5c0a933c0d252a62ae884ce2108e
cclazarou <candacelazarou@gmail.com>
2016-10-22 00:55:59 -0700

    update driver code to test getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -22,6 +22,16 @@ class Santa
     puts "age #{@age}"
   end
 
+#GETTER METHODS
+
+def age
+  @age
+end
+
+def ethnicity
+  @ethnicity
+end
+
 #SETTER METHODS
 
 #age santa by one year
@@ -71,32 +81,42 @@ example_genders.length.times do |i|
   santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
 
-# #create a new instance of the Santa class called candace, then print its instance variables
-# candace = Santa.new("cisgender","filipino")
-# candace.about_instance
+#1. create a new instance of the Santa class called candace
+#2. print its instance variables
+candace = Santa.new("cisgender","filipino")
+candace.about_instance
 
-# #print all members of santas array along with instance variables
-# puts "#{santas}"
+#print all members of santas array along with instance variables
+puts "#{santas}"
 
-# #print Class and address of member of santa array at 0 index
-# puts "#{santas[0]}"
+#print Class and address of member of santa array at 0 index
+puts "#{santas[0]}"
 
-# #get Santa instance at element 0 in santas array, have them celebrate a birthday
-# santas[0].celebrate_birthday
+#1. get Santa instance at index 0 in santas array
+#2. make instance celebrate a birthday
+santas[0].celebrate_birthday
 
-#get Santa instance at element 2 in santas array
-#print santas array
-#make instance get mad at Dancer
-#print santas array again
-# puts santas
-# santas[2].get_mad_at("Vixen")
-# puts santas
+#1. get Santa instance at index 2
+#2. print santas array
+#3. make instance get mad at Vixen
+#4. print santas
+puts santas
+santas[2].get_mad_at("Vixen")
+puts santas
 
-#get Santa instance at element 1 in santas array
-#print santas array
-#reassign gender instance variable
-# puts santas
-# santas[1].reassign_gender("none of your business")
-# puts santas
+#1. print santas array
+#2. reassign gender instance variable of instance at index 1
+#3. print santas array
+puts santas
+santas[1].reassign_gender("none of your business")
+puts santas
 
+#1. get age of instance at index 4
+#2. get ethnicity of instance at index 4
+puts santas[4].age
+puts santas[4].ethnicity
 
+#1. get age of instance at index 0
+#2. get ethnicity of instance at index 0
+puts santas[0].age
+puts santas[0].ethnicity
