fae261d1fad19663f0555dda39e934a4e33bf0eb
cclazarou <candacelazarou@gmail.com>
2016-10-22 18:57:03 -0700

    add initialize method, add instance variables, assign instance variables to attr_accessor

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,3 +1,19 @@
+class GuessWhat
+#attr methods
+attr_accessor :max_guesses
+
+#initialize method (instance variables)
+#set maximum number of guesses to the number of characters in the secret word given by user 1
+  def initialize(user1_input):
+    @secret_word = user1_input
+    @max_guesses = user1_input/user1_input.to_i.length
+    @guess_count = 0
+    @game_is_over = false
+  end
+
+#other methods
+end
+
 #driver code handles user i/o
 #guesses limited, limit related to length of word
 #repeat guesses ok
@@ -5,8 +21,8 @@
 #if user wins congratulate, if user loses taunt
 
 #Class methods
-#1. create a secret word for the user to guess
-#2. use length to create max # of guesses
+#1. save secret word from user 1
+#2. use length of secret word to create max # of guesses
 #3. check user guess against max # of guesses, if reached max # then user loses, taunt user, game ends
 #4. if user is under max #, see if guess is the secret word.  if yes, then user wins, congratulate user, game ends
 #5. if guess is not secret word, see if guess is IN secret word.  if yes, give feedback to user on where the match is within the secret word ("___phrase____")
@@ -15,4 +31,5 @@
 
 #Driver code
 #1. create new game, display rules
-#2. ask user for guess until limit is reached, passing guess to the Class to check and give feedback
+#2. ask user 1 for secret word
+#2. ask user 2 for guesses until limit is reached, passing guess to the Class to check and give feedback each time
