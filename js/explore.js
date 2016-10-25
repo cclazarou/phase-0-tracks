47a962ffd7ab8c2dccaf6475b4844198baff794b
cclazarou <candacelazarou@gmail.com>
2016-10-25 12:32:24 -0700

    add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+/*
+1.put string in variable
+2.make string an array (string can already behave as array in js)
+3.pop a character off the end of the array, then push that character back into the beginning of the array
+4.do that for the length of the original string
+*/
