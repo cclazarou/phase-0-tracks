e6f15885440f2ec69b423d2ab2aaf33c76922d42
cclazarou <candacelazarou@gmail.com>
2016-11-13 14:10:30 -0800

    add event listener to button

--- web_dev/client_side_js/js/main.js
+++ web_dev/client_side_js/js/main.js
@@ -3,3 +3,9 @@ h1Titles = document.getElementsByTagName("h1");
 firstTitle = h1Titles[0];
 
 firstTitle.style.backgroundColor = "red";
+
+buttons = document.getElementsByTagName("button");
+
+firstButton = buttons[0];
+
+firstButton.addEventListener("click", function(){ alert("Aaaaaagh!"); });
